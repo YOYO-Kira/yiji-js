0a7c9af6a4769a9033fe0ba252578fcd541678ca
YOYO-Kira <1164020843@qq.com>
2019-08-29 15:48:52 +0800

    jjjjj

---
+++ precache-manifest.15114c762422e97238cbe98a1ab047ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47e21539e3d15930d4376a1816861824",
+    "url": "/yiji/index.html"
+  },
+  {
+    "revision": "6deae3c5c6ae116b3900",
+    "url": "/yiji/static/css/2.acb72abc.chunk.css"
+  },
+  {
+    "revision": "470a98e668f7534da7e4",
+    "url": "/yiji/static/css/main.06faccb2.chunk.css"
+  },
+  {
+    "revision": "6deae3c5c6ae116b3900",
+    "url": "/yiji/static/js/2.513cd902.chunk.js"
+  },
+  {
+    "revision": "470a98e668f7534da7e4",
+    "url": "/yiji/static/js/main.5e758d08.chunk.js"
+  },
+  {
+    "revision": "b39296794a87484ad183",
+    "url": "/yiji/static/js/runtime~main.48a7557d.js"
+  }
+]);
